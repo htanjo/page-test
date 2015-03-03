bb211bdb56ca7136d2d310051849a5a84ae92a7c
Tanjo, Hiroyuki <expheno@gmail.com>
2015-03-04 00:54:50 +0900

    Test and iterate it using external config

--- test/page-test.js
+++ test/page-test.js
@@ -4,46 +4,69 @@ var assert = require('assert');
 var psi = require('psi');
 var meta = require('../lib/meta');
 
-var url = 'https://github.com/';
+var urls = require('./fixtures/urls.json');
+var expected = require('./fixtures/expected.json');
 
-describe('Page: ' + url, function () {
+urls.forEach(function (url) {
+  describe('URL: ' + url, function () {
 
-  this.timeout(10000);
+    this.timeout(10000);
 
-  describe('PageSpeed Insights', function () {
+    describe('PageSpeed Insights', function () {
 
-    it('says 80 or over', function (done) {
-      psi(url, {strategy: 'desktop'}, function (error, data) {
-        assert(data.score >= 80);
-        done();
+      this.timeout(20000);
+
+      it('says 85 or over', function (done) {
+        psi(url, {strategy: 'desktop'}, function (error, data) {
+          assert(data.score >= 85);
+          done();
+        });
       });
+
+    });
+
+    describe('Meta data', function () {
+
+      var results;
+
+      before(function (done) {
+        meta(url, function (error, data) {
+          results = data;
+          done();
+        });
+      });
+
+      it('contains proper title', function () {
+        var expectation = expected[url] && expected[url].title ? expected[url].title : null;
+        if (expectation !== null) {
+          assert.equal(results.title, expectation);
+        }
+        else {
+          assert(!!results.title);
+        }
+      });
+
+      it('contains proper description', function () {
+        var expectation = expected[url] && expected[url].description ? expected[url].description : null;
+        if (expectation !== null) {
+          assert.equal(results.description, expectation);
+        }
+        else {
+          assert(!!results.description);
+        }
+      });
+
+      it('contains proper keywords', function () {
+        var expectation = expected[url] && expected[url].keywords ? expected[url].keywords : null;
+        if (expectation !== null) {
+          assert.equal(results.keywords, expectation);
+        }
+        else {
+          assert(!!results.keywords);
+        }
+      });
+
     });
 
   });
-
-  describe('Meta data', function () {
-
-    var results;
-
-    before(function (done) {
-      meta(url, function (error, data) {
-        results = data;
-        done();
-      });
-    });
-
-    it('contains proper title', function () {
-      assert.equal(results.title, 'GitHub · Build software better, together.');
-    });
-
-    it('contains proper description', function () {
-      assert.equal(results.description, 'Build software better, together.');
-    });
-
-    it('contains some keywords', function () {
-      assert(results.keywords && results.keywords.length > 0);
-    });
-
-  });
-
 });
